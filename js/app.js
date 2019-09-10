9aa76c518a5d8bc390b71b5c4d2b094b025b1aff
Paul Bennett <luapkb@gmail.com>
2019-09-10 14:57:19 -0700

    sstart

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strtict'
+
+console.log ('hello');
+
+var name = prompt('hello whats your name');
+
+if answer 
+
+var color = prompt('what is your color');
+
+switch(color.toLowerCase()){
+    case 'red':
+        console.log ('you picked red');
+    case 'blue':
+        console.log ('you like blue');
+    default: console.log('you picked diffent');
+
+}
+
+)
+var answer = confirm('are you ready?');
+
+var guess = prompt ('type something')
+
+if (typeof(answer) === "boolean")
+{
+        console.log ('you are a number');
+}else if ()
+}
